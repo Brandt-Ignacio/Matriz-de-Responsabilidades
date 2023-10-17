20fc8a3a52bb789bc6a5bd52a87b77a0ed39f3f5
Brandt-Ignacio <81595011+Brandt-Ignacio@users.noreply.github.com>
2023-10-17 00:08:29 -0300

    Delete src directory

--- src/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dataController = require('../controllers/dataController');
-
-// Ruta para obtener datos desde MongoDB
-router.get('/datos', dataController.obtenerDatos);
-
-module.exports = router;
