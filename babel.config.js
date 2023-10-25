072c98e2c856b2d0e32c138e568c7af093fa6709
Brandt-Ignacio <81595011+Brandt-Ignacio@users.noreply.github.com>
2023-10-25 20:21:30 -0300

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env", // Para transpilar JavaScript moderno
-    "@babel/preset-react" // Para transpilar JSX
-  ],
-  // Otros ajustes y plugins si es necesario
-};
